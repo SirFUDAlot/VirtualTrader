2e07990de6c9a70955caa3f0b4f6ff09615c4e08
Cuonic <cuonic@gmail.com>
2011-07-28 16:46:56 -0700

    getstock.php updated to use Curl instead of simplexml_load_file

--- includes/getstock.php
+++ includes/getstock.php
@@ -3,7 +3,16 @@
 function GetStockInfo($stockname)
 	{
 		$url = "http://www.google.com/ig/api?stock=" . $stockname;
-		$xml = simplexml_load_file($url);
+		
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch,CURLOPT_URL,$url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+		$data = curl_exec($ch);
+		curl_close($ch);
+	
+		$xml = simplexml_load_string($data);
 		$finance = $xml->xpath("/xml_api_reply/finance");
 		
 		$stockinfo['code'] = $finance[0]->symbol['data']; // Stock code name (ex: GOOG)

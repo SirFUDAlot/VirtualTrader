0dd9b9f73d69f791a5402644df0ee4d6fa3a9161
SirFUDAlot <SirFUDalot@hotmail.com>
2011-08-27 23:32:21 +0430

    Edited includes/mysql.php via GitHub

--- includes/mysql.php
+++ includes/mysql.php
@@ -1,14 +1,30 @@
 <?php
 
-$mysql_host = "localhost";
-$mysql_user = "*****";
-$mysql_pass = "*****";
-$mysql_db = "virtualtrader";
+/*
+Site Settings!
+*/
+$title = "Virtual Trader";
+$lang = "en"; //Site language - only en atm.
+$results = 10; //Results per page
 
-$mysql_connect_1 = mysql_connect($mysql_host, $mysql_user, $mysql_pass);
-if(!$mysql_connect_1) { echo "Error encountered connecting to MySQL Server... Exiting script"; exit; }
+/*
+MySQL Settings!
+*/
 
-$mysql_connect_2 = mysql_select_db($mysql_db);
-if(!$mysql_connect_1) { echo "Error encountered selecting database... Exiting script"; exit; }
+mysql_connect("localhost", "Username", "Password") or die("Error! - Failed connecting to MySQL Server.");
+mysql_select_db("Database") or die("Error! - Failed selecting MySQL Database.");
 
+session_start();
+
+/*
+This is a check to make sure a user doesnt access a page he isnt suppose to be able to view
+*/
+
+if(empty($_GET['p']) or !preg_match('/^[A-Za-z0-9]+$/',$_GET['p'])) {
+  $_GET['p'] = "index";
+}
+if(!file_exists("./includes/pages/" . $_GET['p'] . ".php") or !file_exists("./includes/language/" . $lang . "/" . $_GET['p'] . ".php")) {
+	$_GET['p'] = "index";
+}
+include $includeDir . "./includes/language/" . $lang . "/" . $_GET['p'] . ".php";
 ?>
